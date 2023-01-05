6d1179087a1fe3dbc3d8db4b1654dd0e37605b2a
Gary Jackson <gary.jackson@sixpivot.com.au>
2023-01-05 10:15:17 +0200

    Add image writer to view dataset digits

--- src/DigitRecogniserBaseline.ConsoleApp/Program.cs
+++ src/DigitRecogniserBaseline.ConsoleApp/Program.cs
@@ -4,4 +4,13 @@ using DigitRecogniserBaseline.MNIST;
 
 var data = await DataReader.ReadDataAsync();
 
+
+// Test to see the some images
+var count = 0;
+foreach (var item in data.TrainingData.Take(20))
+{
+    await ImageWriter.WriteImageToFileAsync(item.Image, $"{count}-{item.Label}.bmp");
+    count++;
+}
+
 Console.WriteLine("Hello, World!");
---
+++ src/DigitRecogniserBaseline.MNIST/ImageWriter.cs
@@ -0,0 +1,28 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+
+namespace DigitRecogniserBaseline.MNIST
+{
+    public static class ImageWriter
+    {
+        public static async Task WriteImageToFileAsync(int[,] image, string outputFilePath)
+        {
+            var numRows = image.GetLength(0);
+            var numCols = image.GetLength(1);
+
+            var img = new Image<Rgb24>(numCols, numRows);
+
+            for (var row = 0; row < numRows; row++)
+            {
+                for (var col = 0; col < numCols; col++)
+                {
+                    var value = (byte)image[row, col];
+                    img[col, row] = new Rgb24(value, value, value);
+                }
+            }
+
+            await using var stream = File.OpenWrite(outputFilePath);
+            await img.SaveAsBmpAsync(stream);
+        }
+    }
+}
