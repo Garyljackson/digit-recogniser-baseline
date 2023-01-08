e336e1ec87b8d3876a4794a248decb8b8665cbd1
Gary Jackson <gary.jackson@sixpivot.com.au>
2023-01-08 10:55:45 +0200

    Added model to average digits

---
+++ src/DigitRecogniserBaseline.ConsoleApp/ModelTrainer.cs
@@ -0,0 +1,47 @@
+﻿using DigitRecogniserBaseline.MNIST;
+
+namespace DigitRecogniserBaseline.ConsoleApp
+{
+    internal static class ModelTrainer
+    {
+        internal static List<DataItem> Train(DataSet dataSet)
+        {
+            var groupedTrainingData = dataSet.TrainingData.GroupBy(item => item.Label);
+            var averagedItems = new List<DataItem>();
+
+            foreach (var grouping in groupedTrainingData)
+            {
+                var numRows = grouping.First().Image.GetLength(0);
+                var numCols = grouping.First().Image.GetLength(1);
+
+                var averagedImage = new int[numRows, numCols];
+
+                var totalDigits = grouping.Count();
+
+                foreach (var dataItem in grouping)
+                {
+                    for (var row = 0; row < numRows; row++)
+                    {
+                        for (var col = 0; col < numCols; col++)
+                        {
+                            var value = dataItem.Image[row, col];
+                            averagedImage[row, col] += value;
+                        }
+                    }
+                }
+
+                for (var row = 0; row < averagedImage.GetLength(0); row++)
+                {
+                    for (var col = 0; col < averagedImage.GetLength(1); col++)
+                    {
+                        averagedImage[row, col] /= totalDigits;
+                    }
+                }
+
+                averagedItems.Add(new DataItem(grouping.Key, averagedImage));
+            }
+
+            return averagedItems;
+        }
+    }
+}
--- src/DigitRecogniserBaseline.ConsoleApp/Program.cs
+++ src/DigitRecogniserBaseline.ConsoleApp/Program.cs
@@ -1,16 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using DigitRecogniserBaseline.ConsoleApp;
 using DigitRecogniserBaseline.MNIST;
 
 var data = await DataReader.ReadDataAsync();
 
+var averagedDigits = ModelTrainer.Train(data);
 
-// Test to see the some images
-var count = 0;
-foreach (var item in data.TrainingData.Take(20))
+foreach (var item in averagedDigits)
 {
-    await ImageWriter.WriteImageToFileAsync(item.Image, $"{count}-{item.Label}.bmp");
-    count++;
+    await ImageWriter.WriteImageToFileAsync(item.Image, $"{item.Label}.bmp");
 }
 
 Console.WriteLine("Hello, World!");
